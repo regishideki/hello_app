e68e4071395021111f994df0b91a9ac9e7ee8b6d
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2014-11-22 00:54:59 +0000

    improve README file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "Hello, Rapha"
+    render text: "Hello, World!"
   end
   
 end
